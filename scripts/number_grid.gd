b53f09e0e33b2bfb9ac5a046ecfc8e30c3c181eb
SHKBRD <121646151+SHKBRD@users.noreply.github.com>
2025-06-02 11:45:21 -0400

    add progress

--- scripts/number_grid.gd
+++ scripts/number_grid.gd
@@ -7,8 +7,18 @@ static var numpadNumber: PackedScene = preload("res://scenes/numpad_number.tscn"
 @export var height: int = 4
 @export var buttonDistance: float = 100.0
 
+var connectOrder: int = 0
+var currentNumber: int = -1
+var dragging: bool = false
+
 func _ready() -> void:
 	init_numbers()
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("NumpadButtonPressed"):
+		dragging = true
+	else:
+		dragging = false
 
 func init_numbers() -> void:
 	for heightInd: int in height:
@@ -16,5 +26,28 @@ func init_numbers() -> void:
 			var newNumber: NumpadNumber = numpadNumber.instantiate()
 			newNumber.position = Vector2(widthInd*buttonDistance, heightInd*buttonDistance)
 			newNumber.init_numpad_button(heightInd*height + widthInd + 1)
+			newNumber.mouse_hovered.connect(_on_number_just_hovered)
+			newNumber.mouse_left.connect(_on_number_just_leaved)
 			add_child(newNumber)
-			 
+
+func reset_progress() -> void:
+	var numpadButtons: Array = get_children()
+	for numberButton: NumpadNumber in numpadButtons:
+		numberButton.reset()
+	connectOrder = 0
+	currentNumber = -1
+
+func _on_number_just_hovered(numberObj: NumpadNumber) -> void:
+	print()
+	print("ORDER: " + str(connectOrder))
+	print("CURRENT: " + str(currentNumber))
+	currentNumber = numberObj.number
+	
+	if currentNumber == connectOrder + 1:
+		currentNumber = currentNumber
+		numberObj.confirm_number()
+	else:
+		reset_progress()
+
+func _on_number_just_leaved(numberObj: NumpadNumber) -> void:
+	currentNumber = -1
--- scripts/numpad_button.gd
+++ scripts/numpad_button.gd
@@ -1,15 +1,35 @@
 extends Sprite2D
 class_name NumpadNumber
 
+signal mouse_hovered(NumpadNumber)
+signal mouse_left(NumpadNumber)
+
 var number: int = 0
 var readInput: bool = false
+var confirmed: bool = true
 
 func init_numpad_button(num: int) -> void:
 	number = num
 	%Label.text = str(number)
 
+func _update(delta: float) -> void:
+	if confirmed:
+		modulate = Color.GREEN
+	else:
+		modulate = Color.WHITE
+
+func confirm_number() -> void:
+	confirmed = true
+
+func reset() -> void:
+	confirmed = false
+
 func _on_area_2d_mouse_entered() -> void:
 	readInput = true
+	mouse_hovered.emit(self)
+	modulate = Color(0.5, 0.5, 0.5, 0.5)
 
 func _on_area_2d_mouse_exited() -> void:
 	readInput = false
+	mouse_left.emit(self)
+	modulate = Color(1.0, 1.0, 1.0, 1.0)
